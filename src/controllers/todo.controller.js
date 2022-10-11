8c8e7fe741c45fea6030164752589c9fb39e3123
Nilesh Acharya <nileshacharya121@gmail.com>
2022-10-11 21:32:47 +0530

    changes: - for get call pagination and limit added - deletion route - update route added

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -22,8 +22,47 @@ const taskList = new Map();
  *
  */
 TodoController.get = async function (req, res, next) {
+  // if id is provided then we will only return single task.
+  const { id } = req.params;
+  if (id) {
+    if (!taskList.has(id)) {
+      return next(new Error("Unable to get task invalid id"));
+    }
+    return res.status(200).json(taskList.get(id));
+  }
+
+  const totalTodos = taskList.size;
+  let limit = req.query.limit || 10; //default todo limit
+  let page = req.query.page || 0; // default todo page
+
+  let maxPage = Math.ceil(totalTodos / limit) - 1;
+
+  page = page > maxPage ? 0 : page;
+
+  const todoKeys = Array.from(taskList.keys()); //create array key.
+  const elementToReturn = todoKeys.slice(page * limit, page * limit + limit);
+
+  const data = [];
+
+  for (let i = 0; i < elementToReturn.length; i++) {
+    const singleTask = taskList.get(elementToReturn[i]);
+    data.push({
+      ...singleTask,
+      id: elementToReturn[i],
+    });
+  }
+
+  res.status(200).json({
+    data: data,
+    total: totalTodos,
+    limit,
+    page,
+  });
+
+  // now we will limit the result with page
+
   //
-  res.json({ msg: "hi" });
+
   //   return next(new Error("hi how are you"));
 };
 
@@ -62,6 +101,26 @@ TodoController.create = function (req, res, next) {
  *  @param {express.NextFunction} next
  */
 TodoController.update = function (req, res, next) {
+  const { id } = req.params;
+  const { isDone } = req.body;
+
+  //check if valid status is provided.
+  if (typeof isDone !== "boolean") {
+    return next(new Error("Invalid todo `isDone` status provided"));
+  }
+
+  //check if given task exists
+  if (!taskList.has(id)) {
+    return next(new Error("Todo item not found with given id  ", id));
+  }
+
+  const updateTask = taskList.get(id);
+  updateTask.isDone = isDone;
+
+  return res.status(200).json({
+    id,
+  });
+
   // /
 };
 
@@ -73,6 +132,15 @@ TodoController.update = function (req, res, next) {
  * @param {express.NextFunction} next
  */
 TodoController.delete = function (req, res, next) {
+  const { id } = req.params;
+  if (!taskList.has(id)) {
+    return next(new Error("Todo with given id not found"));
+  }
+  taskList.delete(id);
+  return res.status(200).json({
+    id,
+  });
+
   //
 };
 
